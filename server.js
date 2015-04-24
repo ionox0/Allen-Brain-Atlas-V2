4def2b8ef29897bb04ef9ba4bdf610e40d966b39
Ian Johnson <ionox0@gmail.com>
2015-04-23 23:11:42 -0400

    Change static build path

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express'),
   app       = express(),
   port      = process.env.PORT || 3001;
 
-app.use(express.static(__dirname + '/build'));
+app.use(express.static('./app'));
 
 app.listen(port, function(){
   console.log('listening on port ' + port);
